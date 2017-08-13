12a3cb57bbbaad641f3dd675274ee3add1cf186b
Ashwin kumar A <30729600+ASHWIN33@users.noreply.github.com>
2017-08-13 23:38:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/index.html', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
