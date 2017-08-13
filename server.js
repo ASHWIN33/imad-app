f65aef252f0de55f04ce445ea108f40b2db9644c
Ashwin kumar A <30729600+ASHWIN33@users.noreply.github.com>
2017-08-13 23:40:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/', function (req, res) {
+app.get('/article.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
